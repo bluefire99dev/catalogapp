17cda70f93841eaf399478b3fdb658fb9f5352c1
bluefire99dev <bluefire99dev@gmail.com>
2022-09-10 20:16:05 +0530

    6th vx store

--- lib/widgets/home_widgets/Catalog_header.dart
+++ lib/widgets/home_widgets/Catalog_header.dart
@@ -1,7 +1,6 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
-
-import '../../widgets/themes.dart';
 
 class CatalogHeader extends StatelessWidget {
   const CatalogHeader({Key? key}) : super(key: key);
